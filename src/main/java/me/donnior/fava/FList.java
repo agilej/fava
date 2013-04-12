bb7309fb2f31d512da6c5db62e05e2a4b763cad5
Donny Xie <donnior@gmail.com>
2013-04-12 13:11:33 +0800

    add java doc

--- src/main/java/me/donnior/fava/FList.java
+++ src/main/java/me/donnior/fava/FList.java
@@ -5,10 +5,24 @@ import java.util.List;
 
 public interface FList<E> extends List<E>, FCollection<E>{
 	
+    /**
+     * get the element at the given index. 
+     * The index can be negative, then will get element at `size + index` 
+     * @param index
+     * @return
+     */
 	E at(int index);
 	
+	/**
+	 * get the first element in the list. If list is empty the result will be null.
+	 * @return the first element in the list or null if list is empty
+	 */
 	E first();
 	
+	/**
+     * get the last element in the list. If list is empty the result will be null.
+     * @return the last element in the list or null if list is empty
+     */
 	E last();
 	
 	E find(Predict<E> function);
@@ -33,6 +47,11 @@ public interface FList<E> extends List<E>, FCollection<E>{
 	
 	<T> FList<T> collect(Function<E, T> function);
 
+	/**
+	 * add elements to end of the list with iteration
+	 * @param elements
+	 * @return 
+	 */
 	FList<E> push(E...elements);
 	
 	<T> T fold(FoldFunction<E, T> function, T init);
